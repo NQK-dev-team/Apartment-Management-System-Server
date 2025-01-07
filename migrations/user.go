f769107ded18cfc39c7eafde02f5ca3afa7bb1fc
astr0tas <nghia369ty@gmail.com>
2025-01-07 21:51:05 +0700

    update role constraint rule for user table

--- migrations/user.go
+++ migrations/user.go
@@ -3,6 +3,7 @@ package migrations
 import (
 	"api/config"
 	"api/models"
+	"fmt"
 )
 
 type UserMigration struct {
@@ -12,10 +13,17 @@ func NewUserMigration() *UserMigration {
 	return &UserMigration{}
 }
 
+func (m *UserMigration) generateRoleCheckRule() string {
+	adminRole := "(is_owner AND is_manager AND NOT is_customer)"
+	managerRole := "(NOT is_owner AND is_manager AND NOT is_customer)"
+	customerRole := "(NOT is_owner AND NOT is_manager AND is_customer)"
+	return fmt.Sprintf("ALTER TABLE \"user\" ADD CONSTRAINT role_check CHECK (%s OR %s OR %s)", adminRole, managerRole, customerRole)
+}
+
 func (m *UserMigration) Up() {
 	config.DB.AutoMigrate(&models.UserModel{})
 	// Add role check constraint
-	config.DB.Exec("ALTER TABLE \"user\" ADD CONSTRAINT role_check CHECK ((NOT (is_customer AND (is_owner OR is_manager))))")
+	config.DB.Exec(m.generateRoleCheckRule())
 }
 
 func (m *UserMigration) Down() {
