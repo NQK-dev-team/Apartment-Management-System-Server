c4f62d94d6dfb452baf9a8b75e5030b50ff37231
NghiaDuong <101704565+astr0tas@users.noreply.github.com>
2025-01-13 13:31:01 +0700

    Update building_service.go

--- models/building_service.go
+++ models/building_service.go
@@ -5,7 +5,7 @@ type BuildingServiceModel struct {
 	BuildingID int64         `json:"buildingID" gorm:"column:building_id;primaryKey;"`
 	Building   BuildingModel `json:"building" gorm:"foreignKey:building_id;references:id;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
 	Name       string        `json:"name" gorm:"column:name;type:varchar(255);not null;"`
-	Price      float64       `json:"price" gorm:"column:price;type:decimal;not null;"`
+	Price      float64       `json:"price" gorm:"column:price;type:numeric;not null;"`
 }
 
 func (u *BuildingServiceModel) TableName() string {
