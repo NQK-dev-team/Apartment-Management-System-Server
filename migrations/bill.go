3292a78b1d8d5167f2d4c5cff7124031d6c36452
astr0tas <nghia369ty@gmail.com>
2025-01-17 21:55:45 +0700

    change bill status starting value

--- migrations/bill.go
+++ migrations/bill.go
@@ -15,8 +15,8 @@ func NewBillMigration() *BillMigration {
 func (m *BillMigration) Up() {
 	config.DB.AutoMigrate(&models.BillModel{})
 	config.DB.AutoMigrate(&models.ExtraPaymentModel{})
-	config.DB.Exec("ALTER TABLE bill ADD CONSTRAINT bill_status CHECK (status >= 0 AND status <= 3);")
-	config.DB.Exec("ALTER TABLE bill ADD CONSTRAINT bill_payment CHECK ((status=1 AND payment_time IS NOT NULL AND payer_id IS NOT NULL) OR (NOT status=1 AND payment_time NOT NULL AND payer_id NOT NULL));")
+	config.DB.Exec("ALTER TABLE bill ADD CONSTRAINT bill_status CHECK (status >= 1 AND status <= 4);")
+	config.DB.Exec("ALTER TABLE bill ADD CONSTRAINT bill_payment CHECK ((status=2 AND payment_time IS NOT NULL AND payer_id IS NOT NULL) OR (NOT status=2 AND payment_time NOT NULL AND payer_id NOT NULL));")
 }
 
 func (m *BillMigration) Down() {
