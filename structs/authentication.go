2e6d129fe5a531ffe5c0503b002ab4d9523e9a4a
astr0tas <nghia369ty@gmail.com>
2025-01-25 12:03:27 +0700

    Update authentication.go

--- structs/authentication.go
+++ structs/authentication.go
@@ -1,15 +1,15 @@
 package structs
 
 type LoginAccount struct {
-	Email    string `json:"email" validate:"required;"`
-	Password string `json:"password" validate:"required;"`
-	Remember bool   `json:"remember;"`
+	Email    string `json:"email" validate:"required"`
+	Password string `json:"password" validate:"required"`
+	Remember bool   `json:"remember"`
 }
 
 type VerifyToken struct {
-	JWTToken string `json:"jwtToken" validate:"required;"`
+	JWTToken string `json:"jwtToken" validate:"required"`
 }
 
 type RefreshToken struct {
-	RefreshToken string `json:"refreshToken" validate:"required;"`
+	RefreshToken string `json:"refreshToken" validate:"required"`
 }
