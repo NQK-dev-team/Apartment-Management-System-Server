7fbba1c80c85d5f4543c2c17f8985935ef8cbe4e
NghiaDuong <101704565+astr0tas@users.noreply.github.com>
2025-04-01 13:56:37 +0700

    added controllers for add and edit staff apis

--- structs/user.go
+++ structs/user.go
@@ -1,1 +1,40 @@
 package structs
+
+import "mime/multipart"
+
+type NewStaff struct {
+	FirstName  string `form:"firstName" validate:"required"`
+	LastName   string `form:"lastName" validate:"required"`
+	MiddleName string `form:"middleName"`
+	SSN        string `form:"ssn" validate:"required,alphanum,len=12"`
+	OldSSN     string `form:"oldSSN" validate:"alphanum,len=9"`
+	Dob        string `form:"dob" validate:"required"`
+	Pob        string `form:"pob" validate:"required"`
+	Phone      string `form:"phone" validate:"required,alphanum,len=10"`
+	Address    string `form:"address" validate:"required"`
+	Email      string `form:"email" validate:"required,email"`
+	Gender     int    `form:"gender" validate:"required,min=1,max=3"`
+	Schedules  []struct {
+		BuildingID int    `form:"buildingID"  validate:"required"`
+		StartDate  string `form:"startDate"  validate:"required"`
+		EndDate    string `form:"endDate"`
+	} `form:"schedules[]"`
+	ProfileImage  *multipart.FileHeader `validate:"required"`
+	FrontSSNImage *multipart.FileHeader `validate:"required"`
+	BackSSNImage  *multipart.FileHeader `validate:"required"`
+}
+
+type EditStaff struct {
+	Schedules []struct {
+		ID         int64  `form:"id" validate:"required"`
+		BuildingID int    `form:"buildingID"  validate:"required"`
+		StartDate  string `form:"startDate"  validate:"required"`
+		EndDate    string `form:"endDate"`
+	} `form:"schedules[]"`
+	NewSchedules []struct {
+		BuildingID int    `form:"buildingID"  validate:"required"`
+		StartDate  string `form:"startDate"  validate:"required"`
+		EndDate    string `form:"endDate"`
+	} `form:"newSchedules[]"`
+	DeletedSchedules []int64 `form:"deletedSchedules[]"`
+}
