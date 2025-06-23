36bdd5bba65e17bb2c4c2f62ec67731a8b2c39c6
astr0tas <nghia369ty@gmail.com>
2025-06-24 01:24:02 +0700

    Update room_resident.go

--- models/room_resident.go
+++ models/room_resident.go
@@ -1,6 +1,7 @@
 package models
 
 import (
+	"database/sql"
 	"time"
 
 	"gorm.io/gorm"
@@ -8,19 +9,19 @@ import (
 
 type RoomResidentModel struct {
 	DefaultModel
-	FirstName               string    `json:"firstName" gorm:"column:first_name;type:varchar(255);not null;"`
-	MiddleName              string    `json:"middleName" gorm:"column:middle_name;type:varchar(255);"`
-	LastName                string    `json:"lastName" gorm:"column:last_name;type:varchar(255);not null;"`
-	SSN                     string    `json:"ssn" gorm:"column:ssn;type:varchar(12);not null;uniqueIndex:idx_ssn;"`
-	OldSSN                  string    `json:"oldSSN" gorm:"column:old_ssn;type:varchar(9);unique;"`
-	DOB                     string    `json:"dob" gorm:"column:dob;type:date;not null;"`
-	POB                     string    `json:"pob" gorm:"column:pob;type:varchar(255);"`
-	Email                   string    `json:"email" gorm:"column:email;type:varchar(255);not null;uniqueIndex:idx_email;"`
-	Phone                   string    `json:"phone" gorm:"column:phone;type:varchar(10);not null;"`
-	Gender                  int       `json:"gender" gorm:"column:gender;type:int;not null;"`                            // 1: Male, 2: Female, 3: Other
-	RelationWithHouseholder int       `json:"relationWithHouseholder" gorm:"column:relation_with_householder;type:int;"` // 1: Child, 2: Spouse, 3: Parent, null: Other
-	UserAccountID           int64     `json:"userAccountID" gorm:"column:user_account_id;"`
-	UserAccount             UserModel `json:"userAccount" gorm:"foreignKey:user_account_id;references:id;constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
+	FirstName               string         `json:"firstName" gorm:"column:first_name;type:varchar(255);not null;"`
+	MiddleName              string         `json:"middleName" gorm:"column:middle_name;type:varchar(255);"`
+	LastName                string         `json:"lastName" gorm:"column:last_name;type:varchar(255);not null;"`
+	SSN                     string         `json:"ssn" gorm:"column:ssn;type:varchar(12);not null;uniqueIndex:idx_ssn;"`
+	OldSSN                  string         `json:"oldSSN" gorm:"column:old_ssn;type:varchar(9);unique;"`
+	DOB                     string         `json:"dob" gorm:"column:dob;type:date;not null;"`
+	POB                     string         `json:"pob" gorm:"column:pob;type:varchar(255);"`
+	Email                   sql.NullString `json:"email" gorm:"column:email;type:varchar(255);uniqueIndex:idx_email;"`
+	Phone                   sql.NullString `json:"phone" gorm:"column:phone;type:varchar(10);"`
+	Gender                  int            `json:"gender" gorm:"column:gender;type:int;not null;"`                            // 1: Male, 2: Female, 3: Other
+	RelationWithHouseholder int            `json:"relationWithHouseholder" gorm:"column:relation_with_householder;type:int;"` // 1: Child, 2: Spouse, 3: Parent, null: Other
+	UserAccountID           int64          `json:"userAccountID" gorm:"column:user_account_id;"`
+	UserAccount             UserModel      `json:"userAccount" gorm:"foreignKey:user_account_id;references:id;constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
 }
 
 func (u *RoomResidentModel) TableName() string {
