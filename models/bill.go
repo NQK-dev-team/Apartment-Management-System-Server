64137a643eaef22e474e354dfcf930c76f55e5fb
NghiaDuong <101704565+astr0tas@users.noreply.github.com>
2025-06-30 09:34:48 +0700

    Update bill.go

--- models/bill.go
+++ models/bill.go
@@ -9,16 +9,18 @@ import (
 
 type BillModel struct {
 	DefaultModel
-	Period        time.Time           `json:"period" gorm:"column:period;not null;type:date;"`
-	Status        int                 `json:"status" gorm:"column:status;not null;type:int;"` // 1: Unpaid, 2: Paid, 3: Overdue, 4: Processing
-	Note          string              `json:"note" gorm:"column:note;type:varchar(255);"`
-	PaymentTime   sql.NullTime        `json:"paymentTime" gorm:"column:payment_time;type:timestamp with time zone;default:now();"`
-	Amount        float64             `json:"amount" gorm:"column:amount;not null;type:numeric;"`
-	PayerID       int64               `json:"payerID" gorm:"column:payer_id;"`
-	Payer         UserModel           `json:"payer" gorm:"foreignKey:payer_id;references:id;constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
-	ContractID    int64               `json:"contractID" gorm:"column:contract_id;not null;"`
-	ExtraPayments []ExtraPaymentModel `json:"extraPayments" gorm:"foreignKey:bill_id;references:id;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
-	Contract      ContractModel       `json:"contract" gorm:"foreignKey:contract_id;references:id;constraint:OnUpdate:CASCADE,OnDelete:RESTRICT;"`
+	Title                string              `json:"title" gorm:"column:title;not null;type:varchar(255);"`
+	Period               time.Time           `json:"period" gorm:"column:period;not null;type:date;"`
+	Status               int                 `json:"status" gorm:"column:status;not null;type:int;"` // 1: Unpaid, 2: Paid, 3: Overdue, 4: Processing
+	Note                 string              `json:"note" gorm:"column:note;type:varchar(255);"`
+	PaymentTime          sql.NullTime        `json:"paymentTime" gorm:"column:payment_time;type:timestamp with time zone;default:now();"`
+	Amount               float64             `json:"amount" gorm:"column:amount;not null;type:numeric;"`
+	TotalAmountWithExtra float64             `json:"totalAmountWithExtra" gorm:"column:total_amount_with_extra;not null;type:numeric;"`
+	PayerID              int64               `json:"payerID" gorm:"column:payer_id;"`
+	Payer                UserModel           `json:"payer" gorm:"foreignKey:payer_id;references:id;constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
+	ContractID           int64               `json:"contractID" gorm:"column:contract_id;not null;"`
+	ExtraPayments        []ExtraPaymentModel `json:"extraPayments" gorm:"foreignKey:bill_id;references:id;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+	Contract             ContractModel       `json:"contract" gorm:"foreignKey:contract_id;references:id;constraint:OnUpdate:CASCADE,OnDelete:RESTRICT;"`
 	// ExtraPayments []ExtraPaymentModel `json:"extraPayments" gorm:"foreignKey:bill_id,contract_id;references:id,contract_id;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
 }
 
