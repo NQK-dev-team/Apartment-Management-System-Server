8f4f0c84b521595ace620e6a1f4166317bf8678e
astr0tas <nghia369ty@gmail.com>
2025-07-04 08:04:51 +0700

    Update user.go

--- structs/user.go
+++ structs/user.go
@@ -26,21 +26,21 @@ type NewStaff struct {
 }
 
 type NewCustomer struct {
-	FirstName        string `form:"firstName" validate:"required"`
-	LastName         string `form:"lastName" validate:"required"`
-	MiddleName       string `form:"middleName"`
-	SSN              string `form:"ssn" validate:"required,alphanum,len=12"`
-	OldSSN           string `form:"oldSSN" validate:"omitempty,alphanum,len=9"`
-	Dob              string `form:"dob" validate:"required,datetime=2006-01-02,dob_18"`
-	Pob              string `form:"pob" validate:"required"`
-	Phone            string `form:"phone" validate:"required,alphanum,len=10"`
-	PermanentAddress string `form:"permanentAddress" validate:"required"`
-	TemporaryAddress string `form:"temporaryAddress" validate:"required"`
-	Email            string `form:"email" validate:"required,email"`
-	Gender           int    `form:"gender" validate:"required,min=1,max=3"`
-	ProfileImage  *multipart.FileHeader `validate:"required"`
-	FrontSSNImage *multipart.FileHeader `validate:"required"`
-	BackSSNImage  *multipart.FileHeader `validate:"required"`
+	FirstName        string                `form:"firstName" validate:"required"`
+	LastName         string                `form:"lastName" validate:"required"`
+	MiddleName       string                `form:"middleName"`
+	SSN              string                `form:"ssn" validate:"required,alphanum,len=12"`
+	OldSSN           string                `form:"oldSSN" validate:"omitempty,alphanum,len=9"`
+	Dob              string                `form:"dob" validate:"required,datetime=2006-01-02,dob_18"`
+	Pob              string                `form:"pob" validate:"required"`
+	Phone            string                `form:"phone" validate:"required,alphanum,len=10"`
+	PermanentAddress string                `form:"permanentAddress" validate:"required"`
+	TemporaryAddress string                `form:"temporaryAddress" validate:"required"`
+	Email            string                `form:"email" validate:"required,email"`
+	Gender           int                   `form:"gender" validate:"required,min=1,max=3"`
+	ProfileImage     *multipart.FileHeader `validate:"required"`
+	FrontSSNImage    *multipart.FileHeader `validate:"required"`
+	BackSSNImage     *multipart.FileHeader `validate:"required"`
 }
 
 type EditStaff struct {
@@ -49,12 +49,12 @@ type EditStaff struct {
 		ID         int64  `form:"id" validate:"required"`
 		BuildingID int64  `form:"buildingID"  validate:"required"`
 		StartDate  string `form:"startDate"  validate:"required,datetime=2006-01-02"`
-		EndDate    string `form:"endDate"`
-	} `form:"schedules[]"`
+		EndDate    string `form:"endDate" validate:"omitempty,datetime=2006-01-02,gtefield=StartDate"`
+	} `form:"schedules[]" validate:"dive"`
 	NewSchedules []struct {
 		BuildingID int64  `form:"buildingID"  validate:"required"`
 		StartDate  string `form:"startDate"  validate:"required,datetime=2006-01-02"`
-		EndDate    string `form:"endDate"`
-	} `form:"newSchedules[]"`
+		EndDate    string `form:"endDate" validate:"omitempty,datetime=2006-01-02,gtefield=StartDate"`
+	} `form:"newSchedules[]" validate:"dive"`
 	DeletedSchedules []int64 `form:"deletedSchedules[]"`
 }
